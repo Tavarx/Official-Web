080819ce8c87d9256aa19e340e35bed1145325c6
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-04-19 12:32:19 +0200

    add server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
+const express = require("express");
+const app = express();
+var path = require('path');
+const port = 3000;
 
+app.use(express.static('public'))
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+    console.log("User is on page")
+});
+
+app.listen(port, () => {
+    console.log(`Server runing on port ${port}`);
+  });
