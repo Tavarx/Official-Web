06bd13a1fdab4136cc134a60b2e954f560cefb7d
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-04-19 19:19:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = 3000;
 app.use(express.static("public"))
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/home.html'));
+    res.sendFile(path.join(__dirname + '/public/index.html'));
     console.log("User joined page");
 });
 
