ed04a5704554be8ad570db2762255c5376abf2db
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-04-20 16:02:50 +0200

    update

--- server.js
+++ server.js
@@ -5,8 +5,16 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/public')); //Serves resources from public folder
 
-app.get("/", (req, res) => {
-    console.log("User joined page");
+// Parse URL-encoded bodies (as sent by HTML forms)
+app.use(express.urlencoded());
+
+// Parse JSON bodies (as sent by API clients)
+app.use(express.json());
+
+// Access the parse results as request.body
+app.post('/', function(request, response){
+    console.log(request.body.user.name);
+    console.log(request.body.user.email);
 });
 
 app.listen(port, () => {
