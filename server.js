a5e18f9c32ba9536125effe2d0b4f7a1776bacee
Gumernus <70224036+gumernus@users.noreply.github.com>
2021-04-20 16:58:52 +0200

    Post update

--- server.js
+++ server.js
@@ -2,13 +2,15 @@ var express = require('express');
 var app = express();
 var path = require('path');
 const port = 3000;
+var joinCounter = 0;
 
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded());
 app.use(express.json());
 
 app.post('/', (req, res) => {
-    console.log(req.body.user.join);
+    console.log(req.body.user.join, joinCounter, "joined page");
+    joinCounter++;
 });
 
 app.listen(port, () => {
